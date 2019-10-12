d8603cf3fe588d0b99645949cb9f1f548f9c759f
Leonardo MINORA <leonardo.minora@gmail.com>
2019-10-11 23:15:05 -0300

    setup the cors

--- src/server.js
+++ src/server.js
@@ -3,17 +3,27 @@ import { User, Token } from "./models";
 
 const server = new Hapi.Server({
   port: process.env.PORT || 8000,
+  routes: {
+    cors: {
+      origin: ["*"],
+      headers: ["Accept", "Content-type"],
+      additionalHeaders: ["X-Requested-With"]
+    }
+  },
   debug: { request: ["*"] }
 });
 
 const basic_validate = async (request, username, password) => {
-  const user = await User.login(username, password)
+  const user = await User.login(username, password);
 
   if (user == undefined) {
     return { credentials: null, isValid: false };
   }
   const token = Token.add(user);
-  return { credentials: {id: user.oid, username: user.login, token: token}, isValid: true };
+  return {
+    credentials: { id: user.oid, username: user.login, token: token },
+    isValid: true
+  };
 };
 
 const token_validate = async (user, request) => {
@@ -30,8 +40,8 @@ const init = async () => {
   await server.register([require("@hapi/basic"), require("hapi-auth-jwt2")]);
   server.auth.strategy("simple", "basic", { validate: basic_validate });
   server.auth.strategy("token", "jwt", {
-    key: Token.secret, 
-    validate: token_validate, 
+    key: Token.secret,
+    validate: token_validate,
     verifyOptions: { algorithms: ["HS256"] }
   });
 
